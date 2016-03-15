172bc368f9117ab8f71ff9cacf50d3807e8d35cd
IDEA-ARRIBA-REDIL <idea.arriba@gmail.com>
2016-03-15 09:29:56 -0500

    Revert "14-03-2016"
    This reverts commit 85e4f9444299cf608ee22812d5ae8e2ec025a3c5.

--- app/lang/en/asistentes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-    "titulo" => "ASSISTANTS ",
-    "subtitulo" => "Here you will find all registered attendees at church.",
-    'genero'     => "{0} Man|{1} Woman",
-);
--- app/views/includes/lenguaje.php
+++
@@ -1,4 +0,0 @@
-<?php   
-App::setLocale('es'); 
-date_default_timezone_set('America/Bogota');
-?>
